132f8c6ab5e9851600480bb52f49188977e6af36
biruntha K <kbiruntha0504@gmail.com>
2025-07-12 22:58:48 +0530

    added

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
-
+const menuShow=document.querySelectorAll("#nav-links a");
 hamburger.addEventListener('click', () => {
   
     hamburger.classList.toggle('open'); 
     navLinks.classList.toggle('active');
+});
+menuShow.forEach(link => {
+  link.addEventListener("click", () => {
+   navLinks.classList.remove("active");
+     hamburger.classList.remove("open");
+  });
 });
