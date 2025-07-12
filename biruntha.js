5e6715041e3da1c53428846aec13ee518d5ec01a
biruntha K <kbiruntha0504@gmail.com>
2025-07-12 12:41:00 +0530

    created

---
+++ biruntha.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  
+    hamburger.classList.toggle('open'); 
+    navLinks.classList.toggle('active');
+});
